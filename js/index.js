d4db6af7bc96d54d630da9a78921714e0a53cd13
JoshDaBosh <jwanggt@gmail.com>
2018-08-30 19:15:51 -0400

    hipster landing page semi-complete

---
+++ js/index.js
@@ -0,0 +1,28 @@
+/*eslint-env browser*/
+function start() {
+    "use strict";
+    if (document.getElementById) {
+        var el_About = document.getElementById("about");
+        
+        /*set width of certain elements 
+        
+        var toWidth = parseInt(el_About.getBoundingClientRect.width, 10) - 1;
+        
+        document.getElementById("about-inner").style.left = String(toWidth);*/
+        
+        /* make dropdown work */
+        var navRoot = el_About;
+        for (var i=0; i<navRoot.childNodes.length; i++) {
+            var node = navRoot.childNodes[i];
+            if (node.nodeName=="LI") {
+                node.onmouseover=function() {
+                    this.className+=" over";
+                }
+                node.onmouseout=function() {
+                    this.className=this.className.replace(" over", "");
+                }
+            }
+        }
+    }
+}
+window.onload=start;
