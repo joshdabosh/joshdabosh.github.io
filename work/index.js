4e4938af1cd9e3ac3f9432823267d6af727953df
JoshDaBosh <jwanggt@gmail.com>
2018-09-03 01:16:23 -0400

    javascript is not fun

--- work/index.js
+++ work/index.js
@@ -2,14 +2,29 @@
 function resize() {
     "use strict";
     var comWidth = parseInt(document.getElementById("com-contribs").getBoundingClientRect().width, 10);
+    console.log(comWidth)
     
-    if (comWidth < 205) {
+    if (comWidth < 205 && comWidth > 191) {
         document.getElementById("github-projs").style.width = "89.5%";
-    } else{
+        document.getElementById("competitions").style.float = "left";
+        document.getElementById("com-contribs").style.float = "right";
+    } else if (comWidth <= 191){
+        document.getElementById("github-projs").style.width = "89.5%";
+        document.getElementById("competitions").style.maxWidth = "89.5%";
+        document.getElementById("com-contribs").style.maxWidth = "89.5%";
+        
+        document.getElementById("competitions").style.float = "left";
+        document.getElementById("com-contribs").style.float = "left";
+    }
+    else{
         document.getElementById("github-projs").style.width = "50%";
+        document.getElementById("competitions").style.maxWidth = "28%";
+        document.getElementById("com-contribs").style.maxWidth = "28%";
+        document.getElementById("competitions").style.float = "right";
+        document.getElementById("com-contribs").style.float = "right";
     }
     
-    document.getElementById("competitions").style.width = comWidth+"px";
+    //document.getElementById("competitions").style.width = comWidth+"px";
     
 }
 
