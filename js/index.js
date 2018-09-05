4ec1c899ef31e940a50ec6e0819d99586e3fa6eb
JoshDaBosh <jwanggt@gmail.com>
2018-09-05 16:45:25 -0400

    i am a css legend

--- js/index.js
+++ js/index.js
@@ -4,14 +4,6 @@ function start() {
     if (document.getElementById) {
         var el_About = document.getElementById("about");
         
-        /*set width of certain elements
-        
-        var toWidth = parseInt(el_About.getBoundingClientRect.width, 10) - 1;
-        
-        document.getElementById("about-inner").style.left = String(toWidth);
-        
-        */
-        
         /* make dropdown work */
         var navRoot = el_About;
         for (var i=0; i<navRoot.childNodes.length; i++) {
