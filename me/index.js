8b7f1bfe2fc908e769e6bd447f886b91a8a62de3
JoshDaBosh <jwanggt@gmail.com>
2019-07-18 19:32:27 -0400

    content

--- me/index.js
+++
@@ -1,8 +0,0 @@
-/*eslint-env browser*/
-function sizePush() {
-    "use strict";
-    var toSize = parseInt(document.getElementById("footer").getBoundingClientRect().height, 10);
-    document.getElementById("push").style.height = toSize+"px";
-}
-
-window.onload=sizePush();
