fbc1acb462125866f6bb8a748db8df37cdc921cd
JoshDaBosh <jwanggt@gmail.com>
2019-07-19 18:27:46 -0400

    see more for the projects to reduce length texts

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
 function showDescription(e){
-    console.log(e);
+    var parentElement = e.parentElement;
+    
+    var desc;
+    
+    for(var i=0; i < parentElement.childNodes.length; i++){
+        if(parentElement.childNodes[i].className == "project-text"){
+            desc = parentElement.childNodes[i];
+            break;
+        }
+    }
+    
+    
+    if(e.textContent == "View description"){
+
+        desc.style.display = "inline";
+        e.textContent = "Close description";
+        
+    }
+    
+    else{
+        
+        desc.style.display="none";
+        e.textContent="View description";
+        
+    }
+    
+    
 }
